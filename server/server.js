9a650fda50c5e8604fe1723477816784d36fbe8e
vinayak tavatam <107667781+tavatamvinayak@users.noreply.github.com>
2025-09-25 15:55:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,8 +10,9 @@ const orderRoutes = require('./routes/orders');
 const app = express();
 
 app.use(cors({
-            origin: ['http://localhost:3000', 'https://ai-powerd-e-commerce-chatbot-2qqr75cw6-tavatamvinayaks-projects.vercel.app/'],
-  methods: ['GET,POST','PUT','DELETE']
+            origin:"*"
+  //           origin: ['http://localhost:3000', 'https://ai-powerd-e-commerce-chatbot-2qqr75cw6-tavatamvinayaks-projects.vercel.app/'],
+  // methods: ['GET,POST','PUT','DELETE']
         }));
 app.use(express.json());
 
