946e8a31c0129ac123bf2145d70cf5351467a70f
vinayak tavatam <107667781+tavatamvinayak@users.noreply.github.com>
2025-09-25 16:13:27 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL || API_URL,
